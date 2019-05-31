f1a2d3641bc90101e552ed79f2661b6748bb1928
starbuck <mausuf@utexas.edu>
2019-05-30 19:36:37 -0500

    heroku testing1

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var mysql = require("mysql");
 var mysql2 = require("mysql2");
 
 var app = express();
+
+// Made port dynamic for deployment
+var PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded ({
@@ -23,6 +27,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/routes"); // Reference routes.js
 app.use("/", routes); // Reference routes.js
 
-var port = 3000;
-app.listen(port);
-console.log("App connected to port " + port);
+
+app.listen(PORT);
+console.log("App connected to port " + PORT);
