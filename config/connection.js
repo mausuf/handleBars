d854c703bbc2010bef163268eadc6fdeaf2280c6
starbuck <mausuf@utexas.edu>
2019-05-30 19:51:58 -0500

    heroku2

--- config/connection.js
+++ config/connection.js
@@ -14,6 +14,27 @@ connection.connect(function(err) {
 });
 
 
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    })
+};
+
+var mysql = require('mysql');
+
+
+
 // Export Connection from here, and import from orm.js
 module.exports = connection;
 
